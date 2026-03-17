d7beb56310d0bcc06ee251a964fc4c239b2e1a58
piyush-200 <piyushadhikari740@gmail.com>
2026-03-17 12:03:31 +0530

    Update apiConnector.js

--- Client/src/Service/apiConnector.js
+++ Client/src/Service/apiConnector.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({baseURL: "https://client-server-22221.onrender.com"});
+export const axiosInstance = axios.create({baseURL: "https://client-server-22221.onrender.com/api/v1"});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
